ff2efb4d0682abc7907ebdfbee9fd42a9072111f
Aman Kumar <amancollege04@gmail.com>
2025-05-15 22:28:10 +0530

    Lecture 3 completed

---
+++ Lecture3/lec3.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello, Aman Kumar";
+    return 0;
+}
---
+++ Lecture3/lec3.md
@@ -0,0 +1,1 @@
+In this lecture we will study how to write out first C++ code
