f0e11c3fd2b60813801f4165d79a965a0f95c7c2
Aman Kumar <amancollege04@gmail.com>
2025-06-08 14:57:40 +0530

    Lecture 6 completed

---
+++ Lecture6/lec6.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // printing *****
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << "*"<<" ";
+    // }
+
+    // ===================================================
+
+    // printing square * pattern..
+    // for(int r = 0; r <= 6; r++){
+    //     for(int c = 0; c <= 6; c++){
+    //         cout << "*"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // ===================================================
+
+    // printing square with 1..
+    // for(int r = 0; r <= 5; r++){
+    //     for(int c = 0; c <= 5; c++){
+    //         cout<<"1"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // ===================================================
+
+    // printing square with numbers like 1, 2, 3..
+    // for(int r = 1; r <= 5; r++){
+    //     for(int c = 1; c <= 5; c++){
+    //         cout<<c<<" ";
+    //         // cout<<r<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // ===================================================
+
+    // printing square with numbers like 1, 2, 3..
+    // for(int r = 5; r >= 1; r--){
+    //     for(int c = 5; c >= 1; c--){
+    //         // cout<<c<<" ";
+    //         cout<<r<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // ===================================================
+
+    // printing square with row*column..
+    // for(int r = 1; r <= 5; r++){
+    //     for(int c = 1; c <= 5; c++){
+    //         cout<<c*c<<" ";
+    //         // cout<<r*r<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // ===================================================
+
+    // printing a, b, c, d, e in columns..
+    // for(int r = 97; r <= 101; r++){
+    //     for(int c = 1; c <= 5; c++){
+    //         cout<<static_cast<char>(r)<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // ===================================================
+
+    // printing a, b, c, d, e in rows..
+    // for(int r = 1; r <= 5; r++){
+    //     for(int c = 97; c <= 101; c++){
+    //         cout<<static_cast<char>(c)<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    
+    // ===================================================
+
+    // printing 1 to 25 in rows..
+    // for(int r = 1; r <= 5; r++){
+    //     for(int c = 1; c <= 5; c++){
+    //         cout<<r*c<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // ===================================================
+
+    
+
+    return 0;
+}
