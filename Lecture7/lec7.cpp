398562a6d4e6f74850412051876397d74e88479b
Aman Kumar <amancollege04@gmail.com>
2025-06-10 07:04:14 +0530

    Lecture 7 Completed

---
+++ Lecture7/lec7.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // printing right triangle star pattern
+    // for(int r = 1; r <= 5; r++){
+    //     for(int c = 1; c <= r; c++){
+    //         cout << "*" << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // ======================================
+
+    // printing inverted right triangle star pattern
+    // for(int r = 5; r >= 1; r-- ){
+    //     for(int c = 1; c <= r; c++){
+    //         cout << "*" << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // ========================================
+
+    // printing right triangle with numbers pattern
+    // for(int r = 1; r <= 5; r++){
+    //     for(int c = 1; c <= r; c++){
+    //         cout << c << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // ======================================
+
+    // printing right triangle with numbers pattern 1, 22, 333, 4444, 55555
+    // for(int r = 1; r <= 5; r++){
+    //     for(int c = 1; c <= r; c++){
+    //         cout<< r << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // ======================================
+
+    // printing inverted right triangle with numbers pattern
+    // for(int r = 5; r >= 1; r--){
+    //     for(int c = 1; c <= r; c++){
+    //         cout << c << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // ======================================
+    // printing right triangle with numbers pattern 1, 21, 321, 4321, 54321
+    // for(int r = 1; r <= 5; r++){
+    //     for(int c = r; c >= 1; c--){
+    //         cout << c << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // ======================================
+
+    // printing right triangle with numbers pattern a, bb, ccc, dddd, eeeee
+    // for(int i = 1; i <= 5; i++){
+    //     for(int j = 1; j <= i; j++){
+    //         cout << char(96 + i) << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // ======================================
+
+    // printing inverted right triangle with numbers pattern a, bb, ccc, dddd, eeeee
+    // for(int i = 5; i >= 1; i--){
+    //     for(int j = 1; j <= i; j++){
+    //         cout << char(96 + i) << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // ======================================
+
+    // printing inverted triangle with numbers pattern 12345, 1234, 123, 12, 1
+    // for(int r = 5; r >= 1; r--){
+    //     for(int c = 1; c <= r; c++){
+    //         cout<< c << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // ======================================
+    // printing right triangle with numbers pattern 5, 54, 543, 5432, 54321
+    // for(int r = 1; r <= 5; r++){
+    //     for(int c = 5; c >= 5-r+1; c--){
+    //         cout << c << " ";
+    //     }
+    //     cout << endl;
+    // }
+    
+
+
+    return 0;
+}
