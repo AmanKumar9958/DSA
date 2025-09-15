1189e04da5a10a3fdb9455f1d1ff878cbc0f2548
Aman Kumar <151604983+AmanKumar9958@users.noreply.github.com>
2025-09-15 11:20:05 +0530

    Lecture 21 Completed

--- Lecture21/insertionSort.cpp
+++ Lecture21/insertionSort.cpp
@@ -7,17 +7,34 @@ int main(){
 
     int arr[n];
 
-    for(int i=0; i<=n; i++){
+    // Taking array elements input
+    for(int i=0; i<n; i++){
         cout << "Enter the element at index " << i << ": ";
         cin >> arr[i];
     }
 
-    for(int i=0; i<=n; i++){
+    // Printing original array
+    for(int i=0; i<n; i++){
         cout << arr[i] << " ";
     }
 
+    cout << endl;
+
     // Insertion sort algorithm
-    
+    for(int i=1; i<n; i++){
+        for(int j=i; j>0; j--){
+            if(arr[j] < arr[j-1]){
+                swap(arr[j], arr[j-1]);
+            } else {
+                break;
+            }
+        }
+    }
+
+    // Printing sorted array
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
