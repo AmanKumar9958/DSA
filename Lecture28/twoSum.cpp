84bd9531d573381e3406e95befc7b4fb01b400a9
Aman Kumar <151604983+AmanKumar9958@users.noreply.github.com>
2025-10-06 22:46:32 +0530

    Lecture 28: - Two sum done

---
+++ Lecture28/twoSum.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+
+    int sizeOfArray;
+    cout<<"Enter the size of the array: ";
+    cin>>sizeOfArray;
+
+    vector<int> arr(sizeOfArray);
+    cout<<"Enter the elements of the array: ";
+    for(int i=0; i<sizeOfArray; i++){
+        cin>>arr[i];
+    }
+
+    int target;
+    cout<<"Enter the sum target: ";
+    cin>>target;
+
+    int start = 0;
+    int end = sizeOfArray - 1;
+
+    while(start <= end){
+        int sum = arr[start] + arr[end];
+        if(sum == target){
+            cout<<"Pair found: ("<<arr[start]<<", "<<arr[end]<<")"<<endl;
+            start++;
+            end--;
+        } else if(sum < target){
+            start++;
+        } else {
+            end--;
+        }
+    }
+
+    return 0;
+}
