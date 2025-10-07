35784828ad20306e90f5d4b49b6b61cb0f3ac0ed
Aman Kumar <151604983+AmanKumar9958@users.noreply.github.com>
2025-10-07 17:40:14 +0530

    Lecture 28: - Refactor pairDifference logic and add executable file

--- Lecture28/pairDifference.cpp
+++ Lecture28/pairDifference.cpp
@@ -19,10 +19,22 @@ int main(){
     cout<<"Enter the difference target: ";
     cin>>target;
 
-    
+    int start = 0;
+    int end = 1;
 
-
-
+    while(end < sizeOfArray){
+        if(start != end){
+            if(arr[end] - arr[start] == target){
+                cout<<"Pair found: "<<arr[start]<<" and "<<arr[end]<<endl;
+                start++;
+                end++;
+            } else if(arr[end] - arr[start] < target){
+                end++;    // if difference is smaller, move the end pointer to the right
+            } else {
+                start++;  // if difference is greater, move the start pointer to the right
+            }
+        }
+    }
 
     return 0;
 }
